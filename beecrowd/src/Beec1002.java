a59f7411f478560b172f4bbb8c5eb4fb55f1bdee
eldiiar_b <eldiiar_b@auca.kg>
2024-09-13 09:49:49 +0600

    Beec 1002 Accepted

---
+++ beecrowd/src/Beec1002.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Beec1002 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        double r = in.nextDouble();
+        double result = 3.14159 * r * r;
+
+        System.out.printf("A=%.4f\n",result);
+
+
+    }
+}
